ea6c4515d9fe657631a02155a6991140f90fa2bd
神里綾華的狗 <121162902+archie0732@users.noreply.github.com>
2023-10-11 13:03:27 +0800

    Update cpeC.cpp

--- cpeC.cpp
+++ cpeC.cpp
@@ -1,30 +1,23 @@
-#include<iostream>
+#include <iostream>
 
+using namespace std;
 
-using namespace std ;
-
-
-int main ()  {
-
-int t=0,n=0;
-int k=1;
-
-
-cin>>t;
-while (t--)
+int main()
 {
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cout<<k<<" ";
-        k=k+2;
+
+  int t = 0, n = 0;
+  int k = 1;
+
+  cin >> t;
+  while (t--)
+  {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+      cout << k << " ";
+      k = k + 2;
     }
-    
+  }
+  cout << endl;
+  return 0;
 }
-cout<< endl;
-return 0;
-
-
-
-}
-
-
