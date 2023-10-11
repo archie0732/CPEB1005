2db32122c8f7ecb9fb97d92ceb394861c225b907
神里綾華的狗 <121162902+archie0732@users.noreply.github.com>
2023-10-11 13:01:31 +0800

    Update cpeA.cpp

--- cpeA.cpp
+++ cpeA.cpp
@@ -1,36 +1,36 @@
-#include<iostream>
+#include <iostream>
 
+using namespace std;
 
-using namespace std ;
-
-
-
-
-int main (){
-    
-int n =0,t=0;
-int time=0;
-cin>>time;
-
-while (time--)
+int main()
 {
 
-cin>> n>>t;
-int k[1000]={};
-bool f=false;
-for(int i=0;i<n;i++){
-    cin>>k[i];
-    if(k[i]==t){
-        f=1;
-    }
-}
-if (f==1){
-    cout<<"Yes"<<endl;
+  int n = 0, t = 0;
+  int time = 0;
+  cin >> time;
 
-}
-else {
-    cout<<"No"<<endl;
-}
-}
-return 0;
+  while (time--)
+  {
+
+    cin >> n >> t;
+    int k[1000] = {};
+    bool f = false;
+    for (int i = 0; i < n; i++)
+    {
+      cin >> k[i];
+      if (k[i] == t)
+      {
+        f = 1;
+      }
+    }
+    if (f == 1)
+    {
+      cout << "Yes" << endl;
+    }
+    else
+    {
+      cout << "No" << endl;
+    }
+  }
+  return 0;
 }
